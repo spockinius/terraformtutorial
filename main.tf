4ab3c0de70d8e24784c87a5f5fbf4420e59fb74d
katariina <katariina.vakkuri@awacademy.training>
2022-03-05 15:55:59 +0200

    terraform vnet

--- main.tf
+++ main.tf
@@ -14,7 +14,21 @@ provider "azurerm" {
   features {}
 }
 
+# Create resource group
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "westus2"
+  tags = {
+  Environment = "Terraform Getting Started"
+  Team = "DevOps"
+  }
 }
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+    name                = "myTFVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "westus2"
+    resource_group_name = azurerm_resource_group.rg.name
+}
+
